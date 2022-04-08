b66bf9a3fbd59dbb004ae37b8cb53744cd779810
Alexandru Radovici <alexandru.radovici@wyliodrin.com>
2022-04-08 14:39:09 +0300

    parser documentation

--- src/parser/ast.rs
+++ src/parser/ast.rs
@@ -1,30 +1,133 @@
 /// Represents a redirect
 #[derive(Debug)]
 pub enum Redirect {
-    /// > filename or >> filename
+    /// Redirect output (stdout) to a filename
+    ///
+    /// `> filename or >> filename`
     Output { words: WordsPtr, append: bool },
-    /// 2> filename or 2>> filename
+    /// Redirect error (stderr) to a filename
+    ///
+    /// `2> filename or 2>> filename`
     Error { words: WordsPtr, append: bool },
-    /// < filename
+    /// Redirect input from (stdin) a filename
+    ///
+    /// `< filename`
     Input { words: WordsPtr },
-    /// &> filename or &>> filename
+    /// Redirect output (stdout) and error (stderr) to a filename
+    ///
+    /// `&> filename or &>> filename`
     OutputAndError { words: WordsPtr, append: bool },
 }
 
-/// A word (part) of a command or a parameter
+/// Represents a word (part) of a command or a parameter.
+///
+/// Commands the formed out of one or more words. For example
+/// command `ls` is composed out of a single word. The command
+/// `ls${VARIABLE}` is composed out of two words:
+///   - the word `ls`
+///   - the variable VARIABLE
+/// The resulting command is the concatentation of the `ls` word with the value
+/// of the variable VARIABLE
+///
+/// ```rust
+/// format!("ls{}", std::env::var("VARIABLE").unwrap())
+/// ```
 #[derive(Debug)]
 pub enum Word {
-    /// a string
+    /// A word that is the actual String that the user typed
+    ///
+    /// ```bash
+    /// $ top
+    /// ```
+    /// ```
+    /// Word("top"),
+    /// ```
     Word(String),
-    /// a list of parameters that have to be concatenated to
-    /// a single string
+    /// A text that is formed out of several words. This is useful
+    /// for commands and parameters that have spaces in between.
+    ///
+    /// ```bash
+    /// $ "command  with a few    spaces"
+    /// ```
+    /// ```
+    // Quotes(
+    //     [
+    //         Word(
+    //             "command",
+    //         ),
+    //         Word(
+    //             "  ",
+    //         ),
+    //         Word(
+    //             "with",
+    //         ),
+    //         Word(
+    //             " ",
+    //         ),
+    //         Word(
+    //             "a",
+    //         ),
+    //         Word(
+    //             " ",
+    //         ),
+    //         Word(
+    //             "few",
+    //         ),
+    //         Word(
+    //             "    ",
+    //         ),
+    //         Word(
+    //             "spaces",
+    //         ),
+    //     ],
+    // ),
+    /// ```
     Quotes(WordsPtr),
-    /// an env variable that has to be expended (replaced with its value)
+    /// A word that has to be replaced with the value of an environment variable.
+    /// Its value is computed by using the `std::env::var("...")`
+    ///
+    /// ```bash
+    /// $ $CMD
+    ///
+    /// $ ${CMD}
+    /// ```
+    /// ```
+    /// Expand("CMD"),
+    /// ```
     Expand(String),
-    /// a command that has to be executed and replaced with its output
-    /// (with the text that it wrote to stdout)
+    /// A word that has to be replaced with the output (what was written to the
+    /// screen - stdout) of a command.
+    ///
+    /// This usually mean executing the command and redirecting the output to
+    /// a pipe, reading the pipe and then replacing the text.
+    ///
+    /// ```bash
+    /// $ $(echo top)
+    /// ```
+    /// ```
+    /// Execute(
+    ///     SimpleCommand {
+    ///         assignments: [],
+    ///         parameters: [
+    ///             [
+    ///                 Word(
+    ///                     "echo",
+    ///                 ),
+    ///             ],
+    ///             [
+    ///                 Word(
+    ///                     "top",
+    ///                 ),
+    ///             ],
+    ///         ],
+    ///         redirects: [],
+    ///     },
+    /// ),
+    /// ```
     Execute(CommandPtr),
-    /// a redirect
+    /// A word that is a redirect.
+    ///
+    /// Should not be used directly.
     Redirect(RedirectPtr),
 }
 
@@ -45,56 +148,71 @@ pub type WordPtr = Box<Word>;
 /// ```
 pub type Words = Vec<WordPtr>;
 
-/// A heap allocated command or parameter
+/// A heap allocated list of words
 pub type WordsPtr = Box<Words>;
 
+/// A list of assignments
 pub type Assignments = Vec<AssignmentPtr>;
+/// A heap allocated list of assignments
 pub type AssignmentsPtr = Box<Assignments>;
 
+/// A list of parameters
 pub type Parameters = Vec<WordsPtr>;
+/// A heap allocated list of parameters
 pub type ParametersPtr = Box<Parameters>;
 
+/// A heap allocated Redirect
 pub type RedirectPtr = Box<Redirect>;
 
+/// A list of heap allocated redirects
 pub type Redirects = Vec<RedirectPtr>;
+// A heap allocated list of heap allocated redirects
 pub type RedirectsPtr = Box<Redirects>;
 
 #[derive(Debug)]
 
+/// Represents an enivronemnt variable assignment
 pub struct Assignment {
+    /// Variable name
     pub variable: String,
+    /// A list of words that should be assigned to the variable
     pub values: WordsPtr,
 }
 
+/// A heap allocated Assignment
 pub type AssignmentPtr = Box<Assignment>;
 
 /// Represents a command to be run
 #[derive(Debug)]
 pub enum Command {
-    /// a simple command that has variable assignments, parameters and
+    /// A simple command that has variable assignments, parameters and
     /// redirects. parameters\[0\] is the actual command.
     SimpleCommand {
+        /// Environment variables that have to be assigned to the command
+        /// (to be used with `execvpe`)
         assignments: AssignmentsPtr,
+        /// The command (first parameter) and the parameters
         parameters: ParametersPtr,
+        /// The redirects
         redirects: RedirectsPtr,
     },
-    /// two commands connected with a pipe
+    /// Two commands connected with a pipe
     /// ```
     /// execute1 || execute2
     /// ```
     PipeCommand(CommandPtr, CommandPtr),
-    /// two commands that a run sequentially (one after the other one)
+    /// Two commands that a run sequentially (one after the other one)
     /// ```
     /// execute1 ; execute2
     /// ```
     SequentialCommand(CommandPtr, CommandPtr),
-    /// two commands, where the second one is run only if the
+    /// Two commands, where the second one is run only if the
     /// first one returned success (0)
     /// ```
     /// execute1 && execute2
     /// ```
     AndCommand(CommandPtr, CommandPtr),
-    /// two commands, where the second one is run only if the
+    /// Two commands, where the second one is run only if the
     /// first one returned failure (not 0)
     /// ```
     /// execute1 || execute2
